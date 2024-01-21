8c40e0cbc72a029d8e60e04242ca4d9497cdb8f5
Quan Minh <quanminh2019@gmail.com>
2024-01-22 00:31:36 +0700

    final

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,22 +1,28 @@
+import React from 'react';
+import brownImage from './brown.png';
+import greenImage from './green.png';
+
 export default () => (
   <div className="birthdayCard">
     <div className="cardFront">
-      <h3 className="happy">HAPPY BIRTHDAY Love!</h3>
+      <h3 className="happy">HAPPY ANNIVERSARY!</h3>
       <div className="balloons">
         <div className="balloonOne" />
         <div className="balloonTwo" />
         <div className="balloonThree" />
         <div className="balloonFour" />
       </div>
+      <img src={greenImage} alt="Green" />
+
     </div>
     <div className="cardInside">
-      <h3 className="back">HAPPY BIRTHDAY vodoo!</h3>
-      <p>Dear Friend,</p>
-      <p>
-        Happy birthday!! I hope your day is filled with lots of love and
-        laughter! May all of your birthday wishes come true.
+      <h3 className="back">HAPPY ANNIVERSARY</h3>
+      <p className="content">Dear bây bì,</p>
+      <p className="content">
+        Thời gian trôi qua thật nhanh. Trong 7 năm qua, mỗi khoảnh khắc bên em đều đáng nhớ. Chúc mừng kỷ niệm 7 năm yêu nhau, người yêu thương của anh!
       </p>
-      <p className="name">Pawan</p>
+      <img src={brownImage} alt="Brown" />
+      <p className="name">Nguồn: mạng</p>
     </div>
   </div>
 );
